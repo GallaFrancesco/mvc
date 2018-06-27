4d18c67a08f2076f846c5000c2aad125b668868c
GallaFrancesco <francesco.galla3@gmail.com>
2018-06-27 11:49:47 +0200

    formatting, warning

--- src/settings.h
+++ src/settings.h
@@ -2,26 +2,29 @@
  **** MPD options
  */
 static char defHost[] = "192.168.1.131";
+
 static unsigned int defPort = 6600;
+
 // database location, needed for libmpdclient
 static char defDBlocation[] = "/home/francesco/.mpd/mpd.db";
+
 // timeout for MPD connection to fail
 #define TIMEOUT 1000
+
 /*
  **** Visualizer options
  */
-
 // number of samples
 // (a power of 2)
-// NOTE: modifying the original value of 1024
-// might result in messy output
 #define N_SAMPLES 1024
 
 // path to the MPD named pipe
 // (defined: 'mpd.conf')
 #define MPD_FIFO "/tmp/mpd.fifo"
+
 // seconds between each mpd status refresh
 #define STATUS_REFRESH 1
+
 // nuber of buffers whose computation is skipped (cyclically).
 // More skips mean less precision, nicer on older CPUs
 // 0 -> do not skip
@@ -29,6 +32,7 @@ static char defDBlocation[] = "/home/francesco/.mpd/mpd.db";
 // 2 -> process one out of three
 // etc
 #define NICENESS 0
+
 // subtracted to each component (visualized column)
 // adjust according to screen height
 #define Y_CORRECTION 20
